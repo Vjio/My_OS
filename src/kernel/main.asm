b11ba1c3af2f9bffd7a2d93fdacad30eed4e0c5f
Davidescu Victor <davidescuvictor2004@gmail.com>
2026-01-10 11:41:32 +0200

    moved c fat code to asm. moved from bootloader to kernel succesfully

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,67 +1,45 @@
-; loaded in memory here
-org 0x7C00
+org 0x0
 bits 16
 
-; just a shortcut for \n. thank you internet
+
 %define ENDL 0x0D, 0x0A
 
+
 start:
-    jmp main
-
-; Prints a string to the screen
-; Params:
-;   ds:si points to string
-puts:
-    ; push registers that i will modify
-    push si
-    push ax
-    push bx
-    
-.loop:
-    ; loads [si] into al. increments si
-    lodsb
-    or al, al   ; verify if next char is NULL
-    jz .done
-
-    ; BIOS INT 10h, interuption for writting
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
-
-    jmp .loop
-
-.done:
-    pop bx
-    pop ax
-    pop si
-    ret
-
-main:
-
-    ; setup data segmentes
-    mov ax, 0
-    ; can't write to ds(data segment), es dirrectly
-    mov ds, ax
-    mov es, ax
-
-    ; setup stack
-    mov ss, ax          ; move stack segment
-    mov sp, 0x7C00     ; move stack pointer so we don't overwrite
-
-    ; print message
-    mov si, msg_hello
-    call puts
-
-    hlt
+	; print hello world message
+	mov si, msg_hello
+	call puts
 
 .halt:
-    jmp .halt
+	cli
+	hlt
 
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+	; save registers we will modify
+	push si
+	push ax
+	push bx
 
+.loop:
+	lodsb               ; loads next character in al
+	or al, al           ; verify if next character is null?
+	jz .done
 
-msg_hello: db "Hello world!", ENDL, 0
+	mov ah, 0x0E        ; call bios interrupt
+	mov bh, 0           ; set page number to 0
+	int 0x10
 
-; the bios will expect the last 2 bits of the first section (512 bits) of memory to be 5
-; it just looks for this signature so that it knows it is bootable
-times 510-($-$$) db 0
-dw 0AA55h
+	jmp .loop
+
+.done:
+	pop bx
+	pop ax
+	pop si    
+	ret
+
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
